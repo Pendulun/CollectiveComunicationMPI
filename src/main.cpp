263d692165ccb95c8d5846567d1abee2bb2e0e5d
Daniel Campos <danielcampos14@hotmail.com.br>
2021-12-27 13:36:17 -0300

    Refatorando e distribuindo em blocos corretamente

--- src/main.cpp
+++ src/main.cpp
@@ -32,91 +32,123 @@ void getType(   int my_rank,
                     }
                 }
 
-void getAllInputs(int my_rank,
-                int comm_sz,
-                int& type,
-                int& totalNumbers,
-                std::queue<float>& my_numbers){
+int getNumbersExpectedCount(int my_rank, int comm_sz, int& totalNumbers, int* qtNumbersProccesses){
+    int numbersExpectedCount;
+    if(my_rank == 0){
 
-                    getType(my_rank, comm_sz, type);
-
-                    if(my_rank == 0){
-
-                        int numbersPerProccess;
-                        int lastProccessQt = 0;
-                        std::cin>>totalNumbers;
-                        std::cout<<"Quantity of numbers : "<<totalNumbers<<std::endl;
+        std::cin>>totalNumbers;
+        std::cout<<"Quantity of numbers : "<<totalNumbers<<std::endl;
                         
-                        if(totalNumbers < comm_sz){
-                            std::cout<<" There are more proccesses than numbers!\n";
-                            numbersPerProccess = 1;
-                        }else{
-                            numbersPerProccess = (int) totalNumbers/comm_sz;
-                            std::cout<<"Numbers per Proccess: "<<numbersPerProccess<<std::endl;
+        //Existem mais processos do que números
+        if(totalNumbers < comm_sz){
+            std::cout<<" There are more proccesses than numbers!\n";
+                            
+            //Aloca 1 número por processo e 0 aos que não forem necessários
+            int numbersAssignedCount = 0;
+            for(int proccess = 0; proccess < comm_sz; proccess++){
+                if(numbersAssignedCount != totalNumbers){
+                    qtNumbersProccesses[proccess] = 1;
+                    numbersAssignedCount++;
+                }else{
+                    qtNumbersProccesses[proccess] = 0;
+                }
+            }
 
-                            if(numbersPerProccess * comm_sz == totalNumbers){
-                                std::cout<<"Every Proccess will get the same quantity of numbers!\n";
-                                lastProccessQt = numbersPerProccess;
-                            }else{
-                                lastProccessQt = totalNumbers - numbersPerProccess * comm_sz;
-                                std::cout<<"The last proccess will get :"<< lastProccessQt<<" numbers!\n";
-                            }
-                        }
+        }else{//Existem mais números do que processos
+            int minimunNumbersPerProccess = (int) totalNumbers/comm_sz;
+            std::cout<<"Numbers per Proccess: "<<minimunNumbersPerProccess<<std::endl;
 
-                        //Enviar a todo processo a quantidade de números que ele deve esperar
-                        for (int proccessRank = 1; proccessRank< comm_sz; proccessRank++){
-                            if(proccessRank != comm_sz -1){
-                                MPI_Send(&numbersPerProccess, 1 , MPI_INT, proccessRank, SEND_QT_NUMBERS_EXPECTED_TAG, MPI_COMM_WORLD);
-                            }else{
-                                MPI_Send(&lastProccessQt, 1 , MPI_INT, proccessRank, SEND_QT_NUMBERS_EXPECTED_TAG, MPI_COMM_WORLD);
-                            }
-                        }
+            for(int proccess = 0; proccess < comm_sz; proccess++){
+                qtNumbersProccesses[proccess] = minimunNumbersPerProccess;
+            }
 
-                        //Block Partition
-                        int numbersSentPerProccessCount = 0;
-                        int totalNumbersSent = 0;
-                        int dest = 0;
+            //Se todo processo recebeu a mesma quantidade de números
+            if(minimunNumbersPerProccess * comm_sz == totalNumbers){
+                std::cout<<"Every Proccess will get the same quantity of numbers!\n";
 
-                        //Recebe todas as entradas
-                        //Envia para cada processo
-                        for (int i = 0; i<totalNumbers; i++){
-                            float numberRead;
-                            std::cin>>numberRead;
-                            //Se atingiu a quantidade enviada para o processo, muda o processo
-                            if (numbersSentPerProccessCount == numbersPerProccess){
-                                dest++;
-                                numbersSentPerProccessCount = 0;
-                            }
+            }else{//Se existem processos com quantidades diferentes de números
 
-                            std::cout<<"Enviando "<<numberRead<<" para o processo "<<dest<<std::endl;
+                std::cout<<"Every Proccess will get at least "<<minimunNumbersPerProccess<<" numbers!\n";
 
-                            //Se for o próprio processo 0, adiciona no dele
-                            if(dest == 0){
-                                my_numbers.push(numberRead);
-                            }else{ //Se não, envia para o processo específico
-                                MPI_Send(&numberRead, 1, MPI_FLOAT, dest, SEND_INIT_NUM_TAG, MPI_COMM_WORLD);
-                            }
+                //Add one number count to each proccess until all leftovers were assigned
+                int qtLeftOverNumbers = totalNumbers - minimunNumbersPerProccess * comm_sz;
+                int assignedLeftOverNumbersCount = 0;
 
-                            numbersSentPerProccessCount++;
-                            totalNumbersSent++;
+                for(int proccessRank = 0; proccessRank < comm_sz; proccessRank++){
+                    qtNumbersProccesses[proccessRank]++;
+                    assignedLeftOverNumbersCount++;
 
-                        }
-
-                    }else{ // Se não for processo 0
-                        
-                        //Recebe a quantidade de números esperados para esse processo
-                        int numbersExpected = 0;
-                        MPI_Recv(&numbersExpected, 1 , MPI_INT, 0, SEND_QT_NUMBERS_EXPECTED_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                        
-                        //Recebe os dados específicos dessa entrada
-                        float dadoLido = 0.0;
-                        for(int numberCount = 0; numberCount < numbersExpected; numberCount++){
-                            MPI_Recv(&dadoLido, 1, MPI_FLOAT, 0, SEND_INIT_NUM_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                            my_numbers.push(dadoLido);
-                        }
-                        
+                    if(assignedLeftOverNumbersCount == qtLeftOverNumbers){
+                        break;
                     }
                 }
+
+            }
+        }
+
+
+        //Enviar a todo processo a quantidade de números que ele deve esperar
+        numbersExpectedCount = qtNumbersProccesses[0];
+        for (int proccessRank = 1; proccessRank< comm_sz; proccessRank++){
+            MPI_Send(&qtNumbersProccesses[proccessRank], 1 , MPI_INT, proccessRank, SEND_QT_NUMBERS_EXPECTED_TAG, MPI_COMM_WORLD);
+        }
+    }else{
+        //Recebe a quantidade de números esperados para esse processo
+        MPI_Recv(&numbersExpectedCount, 1 , MPI_INT, 0, SEND_QT_NUMBERS_EXPECTED_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+    return numbersExpectedCount;
+}
+
+void getMyNumbers(int my_rank, int comm_sz, int& totalNumbers, std::queue<float>& my_numbers, int* qtNumbersProccesses, int numbersExpected){
+    if(my_rank == 0){
+        //Block Partition
+        int processToSendRank = 0;
+        int numbersSentForProccessCount = 0;
+
+        //Recebe todas as entradas
+        //Envia para cada processo
+        for (int numberReadCount = 0; numberReadCount<totalNumbers; numberReadCount++){
+
+            float numberRead;
+            std::cin>>numberRead;
+            
+            int expectedSentNumberForProccess = qtNumbersProccesses[processToSendRank];
+            //Se atingiu a quantidade enviada para o processo, muda o processo
+            if (numbersSentForProccessCount == expectedSentNumberForProccess){
+                processToSendRank++;
+                numbersSentForProccessCount = 0;
+            }
+
+            std::cout<<"Enviando "<<numberRead<<" para o processo "<<processToSendRank<<std::endl;
+
+            //Se for o próprio processo 0, adiciona no dele
+            if(processToSendRank == 0){
+                my_numbers.push(numberRead);
+            }else{ //Se não, envia para o processo específico
+                MPI_Send(&numberRead, 1, MPI_FLOAT, processToSendRank, SEND_INIT_NUM_TAG, MPI_COMM_WORLD);
+            }
+
+            numbersSentForProccessCount++;
+        }
+
+    }else{ // Se não for processo 0
+        //Recebe os dados específicos dessa entrada
+        float numberRead = 0.0;
+        for(int numberCount = 0; numberCount < numbersExpected; numberCount++){
+            MPI_Recv(&numberRead, 1, MPI_FLOAT, 0, SEND_INIT_NUM_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            my_numbers.push(numberRead);
+        }
+        
+    }
+}
+
+void getAllInputs(int my_rank, int comm_sz, int& type, int& totalNumbers, std::queue<float>& my_numbers){
+    getType(my_rank, comm_sz, type);
+    int qtNumbersProccesses[comm_sz];
+    int numbersExpected = getNumbersExpectedCount(my_rank, comm_sz, totalNumbers, qtNumbersProccesses);
+    getMyNumbers(my_rank, comm_sz, totalNumbers, my_numbers, qtNumbersProccesses, numbersExpected);
+}
 
 int main(int argc, char** argv){
 
