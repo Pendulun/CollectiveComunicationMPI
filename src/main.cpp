4dfffe480791831f65f0811b46f09889caf3a923
Daniel Campos <danielcampos14@hotmail.com.br>
2021-12-28 12:02:15 -0300

    Adicionando comentários no código

--- src/main.cpp
+++ src/main.cpp
@@ -8,42 +8,46 @@
 #define SEND_QT_NUMBERS_EXPECTED_TAG 1
 #define SEND_INIT_NUM_TAG 2
 
-void getType(   int my_rank,
-                int comm_sz,
-                int& type){
-                    if(my_rank == 0){
-                        std::string typeRead;
-                        std::cin>>typeRead;
-                        
-                        if(typeRead.compare("sum") == 0){
-                            type = 0;
-                        }else if (typeRead.compare("time") == 0){
-                            type = 1;
-                        }else{
-                            type = 2;
-                        }
+void getType(int my_rank, int comm_sz, int& type){
+    //Get the type of output
+    //sum (0) or time (1) and send it to all proccesses
+    if(my_rank == 0){
+        std::string typeRead;
+        std::cin>>typeRead;
+        
+        if(typeRead.compare("sum") == 0){
+            type = 0;
+        }else if (typeRead.compare("time") == 0){
+            type = 1;
+        }else{
+            type = 2;
+        }
 
-                        //send type to every other proccess
-                        for (int proccessRank = 1; proccessRank< comm_sz; proccessRank++){
-                            MPI_Send(&type, 1 , MPI_INT, proccessRank, SEND_TYPE_INIT_TAG, MPI_COMM_WORLD);
-                        }
-                    }else{
-                        MPI_Recv(&type, 1 , MPI_INT, 0, SEND_TYPE_INIT_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                    }
-                }
+        //send type to every other proccess
+        for (int proccessRank = 1; proccessRank< comm_sz; proccessRank++){
+            MPI_Send(&type, 1 , MPI_INT, proccessRank, SEND_TYPE_INIT_TAG, MPI_COMM_WORLD);
+        }
+    }else{
+        MPI_Recv(&type, 1 , MPI_INT, 0, SEND_TYPE_INIT_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+}
 
 int getNumbersExpectedCount(int my_rank, int comm_sz, int& totalNumbers, int* qtNumbersProccesses){
+    //Get the quantity of numbers from the user
+    //and calculates how many numbers each proccess will get
+    //via block partitioning and send it to each proccess
     int numbersExpectedCount;
     if(my_rank == 0){
 
         std::cin>>totalNumbers;
         std::cout<<"Quantity of numbers : "<<totalNumbers<<std::endl;
                         
-        //Existem mais processos do que números
+        //There are more proccesses than numbers
         if(totalNumbers < comm_sz){
             std::cout<<" There are more proccesses than numbers!\n";
                             
-            //Aloca 1 número por processo e 0 aos que não forem necessários
+            //Set each proccess with one number until it reaches the necessary amount.
+            //Set 0 numbers for proccesses if needed
             int numbersAssignedCount = 0;
             for(int proccess = 0; proccess < comm_sz; proccess++){
                 if(numbersAssignedCount != totalNumbers){
@@ -54,19 +58,20 @@ int getNumbersExpectedCount(int my_rank, int comm_sz, int& totalNumbers, int* qt
                 }
             }
 
-        }else{//Existem mais números do que processos
+        }else{//There are more numbers than proccesses
             int minimunNumbersPerProccess = (int) totalNumbers/comm_sz;
             std::cout<<"Numbers per Proccess: "<<minimunNumbersPerProccess<<std::endl;
-
+            
+            //Assign the minimun amount of numbers that each proccess will get
             for(int proccess = 0; proccess < comm_sz; proccess++){
                 qtNumbersProccesses[proccess] = minimunNumbersPerProccess;
             }
 
-            //Se todo processo recebeu a mesma quantidade de números
+            //If all proccesses got the same amout of numbers
             if(minimunNumbersPerProccess * comm_sz == totalNumbers){
                 std::cout<<"Every Proccess will get the same quantity of numbers!\n";
 
-            }else{//Se existem processos com quantidades diferentes de números
+            }else{//There will be proccesses with more numbers than others
 
                 std::cout<<"Every Proccess will get at least "<<minimunNumbersPerProccess<<" numbers!\n";
 
@@ -93,7 +98,7 @@ int getNumbersExpectedCount(int my_rank, int comm_sz, int& totalNumbers, int* qt
             MPI_Send(&qtNumbersProccesses[proccessRank], 1 , MPI_INT, proccessRank, SEND_QT_NUMBERS_EXPECTED_TAG, MPI_COMM_WORLD);
         }
     }else{
-        //Recebe a quantidade de números esperados para esse processo
+        //Get the amount of numbers that the proccess will haves
         MPI_Recv(&numbersExpectedCount, 1 , MPI_INT, 0, SEND_QT_NUMBERS_EXPECTED_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
 
@@ -101,39 +106,39 @@ int getNumbersExpectedCount(int my_rank, int comm_sz, int& totalNumbers, int* qt
 }
 
 void getMyNumbers(int my_rank, int comm_sz, int& totalNumbers, std::queue<float>& my_numbers, int* qtNumbersProccesses, int numbersExpected){
+    //Fill the proccess's queue with the amount of numbers expected via block partitioning 
     if(my_rank == 0){
         //Block Partition
         int processToSendRank = 0;
         int numbersSentForProccessCount = 0;
 
-        //Recebe todas as entradas
-        //Envia para cada processo
+        //Get all entries and send then to corresponding proccess
         for (int numberReadCount = 0; numberReadCount<totalNumbers; numberReadCount++){
 
             float numberRead;
             std::cin>>numberRead;
             
             int expectedSentNumberForProccess = qtNumbersProccesses[processToSendRank];
-            //Se atingiu a quantidade enviada para o processo, muda o processo
+            //If the amount sent for proccess its already what it should have gotten
             if (numbersSentForProccessCount == expectedSentNumberForProccess){
                 processToSendRank++;
                 numbersSentForProccessCount = 0;
             }
 
-            std::cout<<"Enviando "<<numberRead<<" para o processo "<<processToSendRank<<std::endl;
+            std::cout<<"Sending "<<numberRead<<" to proccess "<<processToSendRank<<std::endl;
 
-            //Se for o próprio processo 0, adiciona no dele
+            //If its proccess 0, just push it to its queue
             if(processToSendRank == 0){
                 my_numbers.push(numberRead);
-            }else{ //Se não, envia para o processo específico
+            }else{ //Send the number to proccess otherwise
                 MPI_Send(&numberRead, 1, MPI_FLOAT, processToSendRank, SEND_INIT_NUM_TAG, MPI_COMM_WORLD);
             }
 
             numbersSentForProccessCount++;
         }
 
-    }else{ // Se não for processo 0
-        //Recebe os dados específicos dessa entrada
+    }else{
+        //Get assigned numbers and push then to queue
         float numberRead = 0.0;
         for(int numberCount = 0; numberCount < numbersExpected; numberCount++){
             MPI_Recv(&numberRead, 1, MPI_FLOAT, 0, SEND_INIT_NUM_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
@@ -144,10 +149,13 @@ void getMyNumbers(int my_rank, int comm_sz, int& totalNumbers, std::queue<float>
 }
 
 void getAllInputs(int my_rank, int comm_sz, int& type, int& totalNumbers, std::queue<float>& my_numbers){
+    //Gets all inputs from the user and do block partitioning of numbers
+    //Each proccess will have it's own numbers in the queue. A proccess may not be assigned any number
+    //if there are more proccesses than numbers
     getType(my_rank, comm_sz, type);
     int qtNumbersProccesses[comm_sz];
-    int numbersExpected = getNumbersExpectedCount(my_rank, comm_sz, totalNumbers, qtNumbersProccesses);
-    getMyNumbers(my_rank, comm_sz, totalNumbers, my_numbers, qtNumbersProccesses, numbersExpected);
+    int amountNumbersExpected = getNumbersExpectedCount(my_rank, comm_sz, totalNumbers, qtNumbersProccesses);
+    getMyNumbers(my_rank, comm_sz, totalNumbers, my_numbers, qtNumbersProccesses, amountNumbersExpected);
 }
 
 int main(int argc, char** argv){
