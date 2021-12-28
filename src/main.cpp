981bb8f258b6c0a85c0b71e60f7db36a66352e1e
Daniel Campos <danielcampos14@hotmail.com.br>
2021-12-28 12:51:24 -0300

    Calculando quais serão os processos pares

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #define SEND_TYPE_INIT_TAG 0
 #define SEND_QT_NUMBERS_EXPECTED_TAG 1
 #define SEND_INIT_NUM_TAG 2
+#define SEND_QT_PROC_WITH_NUMBERS 3
 
 /**
  * @brief  Get the type of output via users input. "sum" (0) or "time" (1) and send it to all proccesses
@@ -109,13 +110,23 @@ int getNumbersExpectedCount(int my_rank, int comm_sz, int& totalNumbers, int* qt
             }
         }
 
+        std::cout<<"Quantity of proccesses with numbers: "<<qtProccessWithNumbers<<std::endl;
+        
+        //Send to every proccess the amount of proccesses that have numbers assigned
+        for (int proccessRank = 1; proccessRank< comm_sz; proccessRank++){
+            MPI_Send(&qtProccessWithNumbers, 1 , MPI_INT, proccessRank, SEND_QT_PROC_WITH_NUMBERS, MPI_COMM_WORLD);
+        }
 
-        //Enviar a todo processo a quantidade de números que ele deve esperar
+        //Send to every proccess the amount of numbers it should expect
         numbersExpectedCount = qtNumbersProccesses[0];
         for (int proccessRank = 1; proccessRank< comm_sz; proccessRank++){
             MPI_Send(&qtNumbersProccesses[proccessRank], 1 , MPI_INT, proccessRank, SEND_QT_NUMBERS_EXPECTED_TAG, MPI_COMM_WORLD);
         }
     }else{
+
+        //get amount of proccesses that will be assigned numbers
+        //may be fewer than the amount of proccesses available
+        MPI_Recv(&qtProccessWithNumbers, 1 , MPI_INT, 0, SEND_QT_PROC_WITH_NUMBERS, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         //Get the amount of numbers that the proccess will haves
         MPI_Recv(&numbersExpectedCount, 1 , MPI_INT, 0, SEND_QT_NUMBERS_EXPECTED_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
     }
@@ -199,6 +210,25 @@ void getAllInputs(int my_rank, int comm_sz, int& type, int& totalNumbers, std::q
     getMyNumbers(my_rank, comm_sz, totalNumbers, my_numbers, qtNumbersProccesses, amountNumbersExpected);
 }
 
+bool willHaveAPairProccess(const int my_rank, const int comm_sz, const int numberOfProccessesTakenAcount){
+    //if there is a pair amount of proccesses
+    if(numberOfProccessesTakenAcount % 2 == 0){
+        return true;
+    }
+    //there are odd number of proccesses
+    
+
+    //if it is not the last proccess (as it cant have a pair proccess)
+    if(my_rank != numberOfProccessesTakenAcount - 1){
+        return true;
+    }
+
+    //it is the last odd proccess
+    std::cout<<"Proccess "<<my_rank<<" will not have a pair proccess!\n";
+    return false;
+
+}
+
 int main(int argc, char** argv){
 
     auto initTime = std::chrono::high_resolution_clock::now();
@@ -218,7 +248,49 @@ int main(int argc, char** argv){
 
     getAllInputs(world_rank, world_size, type, totalNumbers, my_numbers, qtProccessWithNumbers);
 
-    std::cout<<"Proccess "<<world_rank<<" has "<<my_numbers.size()<<" numbers in total!\n";
+    //Se o processo atual contém numeros
+    if (my_numbers.size() != 0){
+
+        //Se for possível formar um par com ele
+        if(willHaveAPairProccess(world_rank, world_size, qtProccessWithNumbers)){
+            int pairProccessRank = 0;
+            //Dado o seu rank, se comunicar com o seu par
+            //define o rank do seu par
+            if(world_rank % 2 == 0){
+                //My pair proccess its the next rank
+                pairProccessRank = world_rank + 1;
+            }else{
+                //My pair proccess its the previous rank
+                pairProccessRank = world_rank - 1;
+            }
+
+            std::cout<<"Pair assigned: "<<world_rank<<" -- "<<pairProccessRank<<"\n";
+
+            //O número par espera primeiro e o ímpar envia primeiro. Depois o contrário
+
+            //Descobre quantos números tem o seu par
+            if(world_rank % 2 == 0){
+                //recv
+                //send
+            }else{
+                //send
+                //recv
+            }
+
+            //until both have only one number
+            if(world_rank % 2 == 0){
+                //recv
+            }else{
+                //send
+            }
+
+        }else{
+            //Se o processo sobrar, após todo processo trocar informação com seu par
+            //ele deve enviar um número para cada processo
+
+
+        }
+    }
 
     MPI_Finalize();
 
